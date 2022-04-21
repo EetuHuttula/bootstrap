af38d9672d6264d21a3f679329f4d92f573b4e1f
EetuHuttula <100779993+EetuHuttula@users.noreply.github.com>
2022-04-22 00:53:05 +0300

    Product listat laitettu

--- script/scripts.js
+++ script/scripts.js
@@ -20,3 +20,23 @@ function toggle2() {
             cInner.style.display = "none"
         }
 }
+
+//popover 
+
+let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+function toggle2() {
+    let carouselInfo = document.getElementById("carousel");
+    let cInner = document.getElementById("cInner");
+        if(carouselInfo.style.display && cInner.style.display  === "none") {
+            carouselInfo.style.display = "block";
+            cInner.style.display = "block";
+            window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
+        } else {
+            carouselInfo.style.display = "none"
+            cInner.style.display = "none"
+        }
+}
